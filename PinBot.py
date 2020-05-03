20b975bbf4541464a67972363be2a83574462f52
sriramg96 <sriramg96@gmail.com>
2020-05-03 02:35:53 -0400

    Updated code to allow cached messages to be pinned/unpinned

---
+++ PinBot.py
@@ -0,0 +1,41 @@
+import discord
+
+
+# Token will have to be updated accordingly
+TOKEN = ""
+client = discord.Client()
+
+@client.event
+async def on_raw_reaction_add(payload):
+    channel = await client.fetch_channel(payload.channel_id)
+    message = await channel.fetch_message(payload.message_id)
+    emoji = payload.emoji
+    print("Channel: ", channel)
+    print("Message: ", message.content)
+    print("Is pinned: ", message.pinned)
+    print("A reaction has been added: {}".format(str(emoji)))
+    if str(emoji) == "📌" and not message.pinned:
+        print("About to pin message")
+        print(message.type)
+        if message.type == discord.MessageType.default:
+            await message.pin()
+            print("Successfully pinned message")
+
+@client.event
+async def on_raw_reaction_remove(payload):
+    channel = await client.fetch_channel(payload.channel_id)
+    message = await channel.fetch_message(payload.message_id)
+    emoji = payload.emoji
+    print("A reaction has been removed: {}".format(str(emoji)))
+    if str(emoji) == "📌" and message.pinned:
+        pin_check = False
+        for react in message.reactions:
+            if "📌" == react.emoji:
+                pin_check = True
+                break
+        if not pin_check:
+            print("About to unpin message")
+            await message.unpin()
+            print("Successfully unpinned message")
+
+client.run(TOKEN)
